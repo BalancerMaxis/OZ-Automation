743d70e07f164d9d7df3dae1b134e22fa8482d20
Silas Stulz <silas.stulz@gmail.com>
2024-06-25 13:55:17 +0200

    - add chain to tg notification

--- tasks/Base Upkeeper/index.js
+++ tasks/Base Upkeeper/index.js
@@ -48,7 +48,7 @@ exports.handler = async function (credentials, context) {
                     notificationClient.send({
                         channelAlias: 'Hal Maxi Bot (tg)',
                         subject: 'Upkeep performed',
-                        message: 'Transaction hash: ' + tx.hash
+                        message: 'Base Upkeep. Transaction hash: ' + tx.hash
                     });
                 } catch (error) {
                     console.error('Failed to send notification', error);
